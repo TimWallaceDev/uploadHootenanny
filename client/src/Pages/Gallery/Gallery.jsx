3115566c2b6fe3f088999343af54e7b5f1a30485
Joseph Park <josephpark@Josephs-MacBook-Pro.local>
2024-03-21 14:20:30 -0400

    made card component

--- client/src/Pages/Gallery/Gallery.jsx
+++ client/src/Pages/Gallery/Gallery.jsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-
-import axios from "axios"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ImageCard from "../../Components/ImageCard/ImageCard";
+import axios from "axios";
 
 export function Gallery() {
-    const serverUrl = "http://localhost:3000/gallery"
-    const { category } = useParams()
+  const serverUrl = "http://localhost:3000/gallery";
+  const { category } = useParams();
 
-    const [images, setImages] = useState(null)
-    useEffect( () => {
+  const [images, setImages] = useState(null);
+  useEffect(() => {
+    async function getAllImages() {
+      //request all images
+      const response = await axios.get(serverUrl + "/" + category);
+      console.log(response);
+      setImages(response.data);
+    }
+    getAllImages();
+  }, [category]);
 
-        async function getAllImages() {
-            //request all images
-            const response = await axios.get(serverUrl + "/" + category)
-            console.log(response)
-            setImages(response.data)
-        }
-        getAllImages()
-    }, [category])
-
-
-    return (
-        <>
-            <h1>Take a gander at them photos</h1>
-        </>
-    )
+  return (
+    <div>
+      <h1>Take a gander at them photos</h1>
+      {images.map((image) => {
+        return <ImageCard images={images} />;
+      })}
+    </div>
+  );
 }
