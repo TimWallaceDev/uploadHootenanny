1d35f5740a60658f3ff041dd2529b27a82ae020d
Tim Wallace <timwallacedev@gmail.com>
2024-03-21 14:00:17 -0400

    add server response

--- client/src/Pages/Gallery/Gallery.jsx
+++ client/src/Pages/Gallery/Gallery.jsx
@@ -1,3 +1,28 @@
-export function Gallery(){
-    
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+import axios from "axios"
+
+export function Gallery() {
+    const serverUrl = "http://localhost:3000/gallery"
+    const { category } = useParams()
+
+    const [images, setImages] = useState(null)
+    useEffect( () => {
+
+        async function getAllImages() {
+            //request all images
+            const response = await axios.get(serverUrl + "/" + category)
+            console.log(response)
+            setImages(response.data)
+        }
+        getAllImages()
+    }, [category])
+
+
+    return (
+        <>
+            <h1>Take a gander at them photos</h1>
+        </>
+    )
 }
--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import imageData from "../data/images.json" assert {type: "json"}
 
 
 export const gallery = express.Router()
@@ -6,7 +7,14 @@ export const gallery = express.Router()
 
 gallery.get("/:category", (req, res) => {
     //check body for category
+    const {category} = req.params
+    console.log(category)
 
-    //send images
-    res.status(200).send("Heres the images")
+    if (category === "all"){
+        res.status(200).json(imageData)
+    }
+    else {
+        const images = imageData.filter(image => image.category === category)
+        res.status(200).json(images)
+    }
 })
